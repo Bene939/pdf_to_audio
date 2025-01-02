704869eb37bb3edb9c234036cd573766e9853f47
Benedikt <bwillecke3@gatech.edu>
2025-01-02 10:03:19 +0100

    add pdf to text

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pdf_to_audio
+In this repo my goal is to take a pdf and generate an audiobook.
---
+++ pdf_to_text.py
@@ -0,0 +1,11 @@
+import pdfplumber
+def pdf_to_text(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        full_text = ""
+        for page in pdf.pages:
+            full_text += page.extract_text() or ""  # Handle cases where text extraction might fail
+
+    # Print or save the extracted text
+    print(full_text)
+
+pdf_to_text("test.pdf")
